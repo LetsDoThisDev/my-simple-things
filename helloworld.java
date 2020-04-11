f510a279253f0ca5d9fc795d395aa287f691c795
avi <dev2nag@gmail.com>
2020-04-11 06:22:20 +0000

    added new print statement

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
 // this is the simple java code to print hello java 
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");  
+     System.out.println("Hello Java");
+     System.out.println("Hello world");  
     }  
 }  
