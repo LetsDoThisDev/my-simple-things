5ac1dcdd7c491b7a3b3598dd642b0c1d202381ad
avi <dev2nag@gmail.com>
2020-04-11 07:07:25 +0000

    modified file for checkout

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,5 @@
-// this is the simple java code to print hello java 
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java");
-     System.out.println("Hello world");  
+     System.out.println("Hello Java");  
     }  
 }  
